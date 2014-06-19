841b1a47e1076aa56063d7d2c42b33aa0fd1e4ed
Ramona Secci <ramonasecci@live.it>
2014-06-19 06:15:29 -0400

    corretto indentazione

--- calcolatore.py
+++ calcolatore.py
@@ -18,25 +18,23 @@ class Calcolatore(object):
     def calcola_massimo(self, colonna):
         if colonna in self.datifile:
             vmax = max(colonna)
-		else:
-			raise ValueError()
-		return vmax
-			
-	
+        else:
+            raise ValueError()
+        return vmax
 
     def calcola_minimo(self, colonna):
         if colonna in self.datifile:
-		    vmin = min(colonna)
-		else:
-		    raise ValueError ()
-	    return vmin 
+            vmin = min(colonna)
+        else:
+            raise ValueError ()
+        return vmin
 
     def calcola_media(self, colonna):
-	if colonna in self.datifile:
-	    somma = sum(colonna)		
-	else:
-	    raise ValueError()
-	media1 = float(somma) / len(colonna)
+        if colonna in self.datifile:
+            somma = sum(colonna)
+        else:
+            raise ValueError()
+        media1 = float(somma) / len(colonna)
         return media1
 
 
