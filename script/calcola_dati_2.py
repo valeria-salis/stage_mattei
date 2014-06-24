3279bbda0b3ac84a5207981844d497b049a16782
Valeria Salis <valesalis10@hotmail.it>
2014-06-24 05:57:19 -0400

    Eseguita modifica sul controllo dell'esistenza del file in scrittura

--- script/calcola_dati_2.py
+++ script/calcola_dati_2.py
@@ -2,10 +2,13 @@
 
 import sys
 from calcolatore import Calcolatore
-
+import os
 def elabora(filename, outfile):
-    c = Calcolatore(filename)
-    c.stampa_calcoli(outfile)
+    if os.path.exists(outfile):
+        raise ValueError("Il file esiste")
+    else:
+        c = Calcolatore(filename)
+        c.stampa_calcoli(outfile)
 
 def main():
     elabora(sys.argv[1], sys.argv[2])
