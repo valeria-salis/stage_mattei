d5c4504c01c44387f1018011c828364545f67ad2
Ramona Secci <ramonasecci@live.it>
2014-06-25 08:54:36 -0400

    Aggiunto argumentparser

--- script/calcola_dati_2.py
+++ script/calcola_dati_2.py
@@ -1,39 +1,20 @@
 #!/usr/bin/python
 
 import sys
-from calcolatore import Calcolatore
+from calcolatore \
+    import Calcolatore
+import argparse
 
 def elabora(filename, outfile):
     c = Calcolatore(filename)
     c.stampa_calcoli(outfile)
 
 def main():
-    elabora(sys.argv[1], sys.argv[2])
-
+    parser = argparse.ArgumentParser(description="Calcola le statistiche da un file in input")
+    parser.add_argument("--input", help="file in input",
+                        type=str, required=True)
+    parser.add_argument("--output", help="file di output",
+                        type=str, required=True)
+    args= parser.parse_args()
+    elabora(args.input, args.output)
 main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
